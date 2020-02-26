d0f903e51bcbb05a9431059b3860325a74acf0d4
usahappy <nskelley9@gmail.com>
2020-02-26 17:09:58 -0500

    Add files via upload

--- action.js
+++ action.js
@@ -2,8 +2,23 @@ $(document).ready(function(){
     // setting headline height
     var navHeight = $(".navbar").height();
     var headlineHeight = $(".headline").height();
-    
     var headlineNormalHeight = headlineHeight - navHeight;
-    
     $(".headline").css("height",headlineNormalHeight);
+    
+    // sticking the navbar
+    var navOrigin = $(".navbar").offset().top;
+    
+    $(window).on("scroll",function(){
+        var scrollTop = $(window).scrollTop();
+        var navTop = $(".navbar").offset().top;
+        
+        if (navTop <= scrollTop) {
+            $(".navbar").addClass("sticky");
+            $(".navbar + .content-container").css("padding-top",navHeight);
+        }
+        if (navOrigin >= scrollTop) {
+            $(".navbar").removeClass("sticky");
+            $(".navbar + .content-container").css("padding-top","0");
+        }
+    });
 });
