243ad7526c602cd1a3cf202c3d36b3c19d6097c2
usahappy <nskelley9@gmail.com>
2020-02-26 19:56:44 -0500

    Add files via upload

--- action.js
+++ action.js
@@ -8,17 +8,37 @@ $(document).ready(function(){
     // sticking the navbar
     var navOrigin = $(".navbar").offset().top;
     
+    $(".content-container").each(function(){
+        $(this).data("unscrolled",true);
+    });
+    
     $(window).on("scroll",function(){
         var scrollTop = $(window).scrollTop();
+        var scrollBottom = scrollTop + $(window).height();
+        
         var navTop = $(".navbar").offset().top;
         
         if (navTop <= scrollTop) {
             $(".navbar").addClass("sticky");
             $(".navbar + .content-container").css("padding-top",navHeight);
         }
-        if (navOrigin >= scrollTop) {
+        if (navOrigin > scrollTop) {
             $(".navbar").removeClass("sticky");
             $(".navbar + .content-container").css("padding-top","0");
         }
+        
+        
+        $(".content-container").each(function() {
+            var currentTop = $(this).offset().top;
+            var currentHeight = $(this).height();
+            
+            if (currentTop <= (scrollBottom - (currentHeight*.5)) && $(this).data("unscrolled")) {
+                $(this).data("unscrolled",false);
+                $("html, body").animate({
+                    scrollTop: $(this).offset().top - navHeight
+                },1500);
+            }
+        });
     });
+    
 });
