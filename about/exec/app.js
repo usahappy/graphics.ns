e76bc4a111aa306d1a51611ddd8a855e44b6a963
usahappy <nskelley9@gmail.com>
2020-03-10 00:29:38 -0400

    Add files via upload

--- about/exec/app.js
+++ about/exec/app.js
@@ -8,5 +8,25 @@ $(document).ready(function(){
     var headlineHeight = "calc(100vh - " + dividerHeight + "px)";
     $(".headline").css("height",headlineHeight);
     
+    var mobileMenuToggle = false;
+    var mobileMenuHeight = $(".mobile-navbar #menu").height();
+    var mobileTopHeight = $(".mobile-navbar #top").height();
+    $(".mobile-navbar #menu").css("height", "calc(100% - " + mobileTopHeight + "px)");
+    $(".mobile-navbar #menu").css("top", "-" + mobileMenuHeight + "px");
     
+    $(".mobile-navbar #top #toggler").change(function(event){   
+        if (mobileMenuToggle) {
+            mobileMenuToggle = false;
+            $("body").css("overflow","auto");
+            $(".mobile-navbar #menu").animate({
+                top: "-" + mobileMenuHeight + "px"
+            }, 200);
+        } else if (!mobileMenuToggle) {
+            mobileMenuToggle = true;
+            $("body").css("overflow","hidden");
+            $(".mobile-navbar #menu").animate({
+                top: mobileTopHeight
+            }, 200);
+        }
+    });
 });
