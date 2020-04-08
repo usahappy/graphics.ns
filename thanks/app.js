64280504e80a8ac50f895628593058c5a9968be2
usahappy <nskelley9@gmail.com>
2020-04-08 18:25:09 -0400

    Add files via upload

--- thanks/app.js
+++ thanks/app.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     console.log("hello world");
     console.log($(window).scrollTop());
     
-    $.getJSON("cards.json", function(data){
-        console.log("test");
-        console.log(data.cards);
+    $.getJSON("https://rha.nicholasskelley.com/thanks/cards.json", function(data){
+        $.each(data.card, function(key,val){
+            var toAppend = "<div class='card'><p id='message'>"+val.message+"</p><p id='name'>"+val.name+"</p><p id='school'>"+ val.school +"</p><p id='gradYear'>"+val.gradYear+"</p></div>";
+            $(".cards-container").append(toAppend);
+        });
     });
 });
