f174776e46342771bab1bf3b76017167a7c49be3
usahappy <nskelley9@gmail.com>
2020-04-08 18:33:36 -0400

    Add files via upload

--- thanks/app.js
+++ thanks/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-    console.log("hello world");
-    console.log($(window).scrollTop());
-    
     $.getJSON("https://rha.nicholasskelley.com/thanks/cards.json", function(data){
         $.each(data.card, function(key,val){
             var toAppend = "<div class='card'><p id='message'>"+val.message+"</p><p id='name'>"+val.name+"</p><p id='school'>"+ val.school +"</p><p id='gradYear'>"+val.gradYear+"</p></div>";
             $(".cards-container").append(toAppend);
         });
+    });
+    
+    window.matchMedia("(prefers-color-scheme:dark)").addListener(function(e) {
+        if (e.matches) {
+            var newIcon = '<link rel="icon" href="images/white-brandmark@1k.png">';
+            $("head").append(newIcon);
+        } else if (!e.matches) {
+            var newIcon = '<link rel="icon" href="images/black-brandmark@1k.png">';
+            $("head").append(newIcon);
+        }
     });
 });
